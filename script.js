f93c9db27addc96418bb1f6d999b2cfcd1cf3a7a
Saumdel <61201958+Saumdel@users.noreply.github.com>
2020-03-02 13:54:12 -0500

    Update

---
+++ script.js
@@ -0,0 +1,33 @@
+// Setting constants //
+const players = {
+    '1': {
+        name: '',
+        score: 0
+    },
+    '-1': {
+        name: '',
+        score: 0
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", ready())
+} else {
+    ready();
+}   
+
+function ready() {
+
+}
