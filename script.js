64c2906941a6c357149a270af1de0a060c47d33d
Saumdel <61201958+Saumdel@users.noreply.github.com>
2020-03-06 11:44:05 -0500

    Update changes

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
-let card = document.getElementsByClassName(`card`);
+let card = document.getElementsByClassName("card");
 let cards = [...card];
-let deck = document.getElementsByTagName(`deck`);
+let deck = document.getElementsByTagName("deck");
 let openedCards = [];
-let timeRemaining = document.getElementById(`time-remaining`);
+let timeRemaining = document.getElementById("time-remaining");
 
-flipCard();
-
-cardEl.addEventListener('click', cardOpen());
-
+card[0].addEventListener('click', flipCard);
+card[1].addEventListener('click', flipCard);
+card[2].addEventListener('click', flipCard);
+card[3].addEventListener('click', flipCard);
+card[4].addEventListener('click', flipCard);
+card[5].addEventListener('click', flipCard);
+card[6].addEventListener('click', flipCard);
+card[7].addEventListener('click', flipCard);
+card[8].addEventListener('click', flipCard);
+card[9].addEventListener('click', flipCard);
+card[10].addEventListener('click', flipCard);
+card[11].addEventListener('click', flipCard);
+card[12].addEventListener('click', flipCard);
+card[13].addEventListener('click', flipCard);
+card[14].addEventListener('click', flipCard);
+card[15].addEventListener('click', flipCard);
 
 function flipCard() {
-  console.log("flipCard");
-  this.openedCards.push(card);
-  if (this.card.type === "chand") {
-    card.style.backgroundImage = url('/assets/images/Chandler.png')
-  } else if (this.card.type === "mon") {
-    card.style.backgroundImage = url(`assets\images\Monica.png`)
-  } else if (this.card.type === "rachel") {
-    card.style.backgroundImage =url(`assets\images\Rachel.png`)
-  } else if (this.card.type === "phoebe") {
-    card.style.backgroundImage = url(`assets\images\Phoebe.png`) 
-  } else if (this.card.type === "ross") {
-    card.style.backgroundImage = url(`assets\images\Ross.png`)
-  } else if (this.card.type === "gun") {
-    card.style.backgroundImage = url(`assets\images\Gunther.jpg`)
-  } else if (this.card.type === "janice") {
-    card.style.backgroundImage = url(`assets\images\Janice.jpg`)
+  console.log(this);
+  if (this.className === "card chand") {
+    this.style.backgroundImage =  'url("assets/images/Chandler.png")'
+  } else if (this.className === "card mon") {
+    this.style.backgroundImage = 'url("assets/images/Monica.png")'
+  } else if (this.className === "card rachel") {
+    this.style.backgroundImage = 'url("assets/images/Rachel.png")'
+  } else if (this.className === "card phoebe") {
+    this.style.backgroundImage = 'url("assets/images/Phoebe.png")'
+  } else if (this.className === "card ross") {
+    this.style.backgroundImage = 'url("assets/images/Ross.png")'
+  } else if (this.className === "card gun") {
+    this.style.backgroundImage = 'url("assets/images/Gunther.jpg")'
+  } else if (this.className === "card janice") {
+    this.style.backgroundImage = 'url("assets/images/Janice.jpg")'
   } else 
-    card.style.backgroundImage = url(`assets\images\Joey.png`)
-};
-
-function getCardType(card) {
-  return card.getElementsByClassName('card')[i];
+    this.style.backgroundImage = 'url("assets/images/Joey.png")'
 };
 
 
-function cardOpen(){
-  openedCards.push(this);
-  if(openedCards[0].type === openedCards[1].type){
-    matchedCards.push(this)
-    matchedCards();
-    console.log("You've got a match!")
-  } else  {
-    unmatchedCards();
-    console.log("Woops!")
-    this.openedCards.pop;
-    this.openedCards.pop;
-  }
-};
+// function cardOpen(){
+//   openedCards.push(this);
+//   if(card.openedCards[0] === card.openedCards[1]){
+//     matchedCards.push(this)
+//     matchedCards();
+//     alert("You've got a match!")
+//   } else  {
+//     unmatchedCards();
+//     alert("Woops!")
+//     this.openedCards.pop;
+//     this.openedCards.pop;
+//   }
+// };
 
 function unmatchedCards(){
   openedCards[0].classList.add("unmatched");
@@ -61,9 +68,7 @@ function matchedCards() {
 
 
 
-//Shuffle//
-
-
+// //Shuffle//
 function shuffleCards(cards) {
   for (let i = cardsArray.length - 1; i > 0; i--) {
     let randIndex = Math.floor(Math.random() * (i + 1));
@@ -89,8 +94,3 @@ function countDown() {
     alert("Oh no! Looks like we cant be friends!")
   }
 };
-
-
-
-
-window.onload.startCountdown();
